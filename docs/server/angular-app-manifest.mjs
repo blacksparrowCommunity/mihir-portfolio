f6ce7a205505f15aacbc3706c08f297d9e4cd638
Mihir Joshi <bsscommunity7@gmail.com>
2026-02-24 15:31:28 +0530

    Update projects page and republish site build

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -33,13 +33,13 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 26374, hash: '8d0cece485948c8e2e28b22ee8e9774fb6c0509247b4f91626efbd5aa3de640e', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 18783, hash: 'f10898695e8a9f81ce378b44c6723e8c22c3f6a8d9e3b9ed5d64ad101173b531', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'about/index.html': {size: 38784, hash: 'b0e60a59d8b44955c16ed7b36236628123ad66ae467a53757fbc54f95e60f5ba', text: () => import('./assets-chunks/about_index_html.mjs').then(m => m.default)},
-    'contact/index.html': {size: 39337, hash: 'd003fe3abc03ddc4396d4fa1f797223d6dadfde4c0056b1d611b7e8f37c4a648', text: () => import('./assets-chunks/contact_index_html.mjs').then(m => m.default)},
-    'index.html': {size: 39263, hash: '60b1ab126befb4fc7cf922706a6f68865ebb98cd8ac2ff2b4d68b7d06ebdf283', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'skills/index.html': {size: 40192, hash: 'd57e00b690622d9261325f0f27efd0a30ed20102d88ec418153563bccad3174e', text: () => import('./assets-chunks/skills_index_html.mjs').then(m => m.default)},
-    'projects/index.html': {size: 42945, hash: '8f704772e75a0057873150ebd7557993d846211df0b65f233bb789714991fa6d', text: () => import('./assets-chunks/projects_index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 26374, hash: '4adb6f3ba02a57d83cb508bd9e21a003ce14ec968d869e94a34ea7cde8d168b8', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 18783, hash: '67717e676a9a2c158d949f245a1101e19af837f8d6017dbd71e797dcddcd0032', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'about/index.html': {size: 38784, hash: '4843ff90a6f89af977b62232437b8c392b5d37a569c5ebc30f17d33d4834e396', text: () => import('./assets-chunks/about_index_html.mjs').then(m => m.default)},
+    'index.html': {size: 39263, hash: '9e8f3f7c50b37ca8486a71de9040820f8d2455fb5c66c7ffe5526fab1a1aa273', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'contact/index.html': {size: 39337, hash: 'a603cdca902c8e2e8014715b536ba8158f49dafc98ff3e7de7a8777dd1169512', text: () => import('./assets-chunks/contact_index_html.mjs').then(m => m.default)},
+    'skills/index.html': {size: 40192, hash: '9bfa9d1ef898295e008b9dcaf65ad1648ba4e9b8588a80690178190ce6a113de', text: () => import('./assets-chunks/skills_index_html.mjs').then(m => m.default)},
+    'projects/index.html': {size: 42947, hash: '76486260b939c9ffdc9b9808249a46584f8e39193d112d0aa1291faface0dc17', text: () => import('./assets-chunks/projects_index_html.mjs').then(m => m.default)},
     'styles-QJLN2PNI.css': {size: 8408, hash: 'wJMYKVUGELY', text: () => import('./assets-chunks/styles-QJLN2PNI_css.mjs').then(m => m.default)}
   },
 };
